d6ccd5464311eb82982c28ad87928557a9c71d38
olaf-snow <olaf.snow@navercorp.com>
2021-02-07 11:11:07 +0900

    page 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import BoxMaker from './box-maker'
+import App from './app'
 
-ReactDOM.render(<BoxMaker />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
